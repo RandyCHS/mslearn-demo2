c9c48cc0d0915038cf4638d768c6471f77d5e25e
Randy Macdonald <jrandy.macdonald@gmail.com>
2022-06-27 15:48:20 -0700

    converse of fall_distance

---
+++ fall_time.py
@@ -0,0 +1,8 @@
+def fall_time(distance):
+    """
+    Calculates the time it takes an object fall a given distance.
+    Parameters:
+        the distance (in meters) travelled by the object.
+    Returns:
+        time: the time (in seconds) it takes an object fall a given distance.
+    """
